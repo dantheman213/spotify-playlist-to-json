de95c5dff14183d3b1be678e666a043f5335791d
Daniel Gillespie <daniel.gillespie0949@gmail.com>
2019-05-30 15:23:17 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ app.get('/playlist/:id', (req, res) => {
     const spotifyId = req.params.id.indexOf('?') > -1 ? req.params.id.substr(0, req.params.id.indexOf('?')) : req.params.id;
 
     (async () => {
-        const tracks = await SpotifyScraper.getPlaylist(spotifyId);
-        res.json(tracks);
+        const playlist = await SpotifyScraper.getPlaylist(spotifyId);
+        res.json(playlist);
     })();
 });
 
