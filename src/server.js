22db644eb035e52c402486635ba6de3d71138bb1
Dan Gillespie <daniel.gillespie@pluto.tv>
2020-01-18 16:39:19 -0800

    works but has issues with more than one concurrent job

--- src/server.js
+++ src/server.js
@@ -5,13 +5,60 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/playlist/:id', (req, res) => {
-    const spotifyId = req.params.id.indexOf('?') > -1 ? req.params.id.substr(0, req.params.id.indexOf('?')) : req.params.id;
+const jobs = new Map();
 
-    (async () => {
-        const playlist = await SpotifyScraper.getPlaylist(spotifyId);
-        res.json(playlist);
-    })();
+app.get('/playlist/query/:id', (req, res) => {
+    const spotifyPlaylistId = req.params.id.indexOf('?') > -1 ? req.params.id.substr(0, req.params.id.indexOf('?')) : req.params.id;
+
+    if (jobs.has(spotifyPlaylistId)) {
+        res.status(400).json({found: true, status: 'playlist already exists in memory. try getting result instead.'});
+        return;
+    }
+
+    jobs.set(spotifyPlaylistId, false);
+
+    (async (spotifyPlaylistId) => {
+        const playlist = await SpotifyScraper.getPlaylist(spotifyPlaylistId);
+        jobs.set(spotifyPlaylistId, playlist);
+    })(spotifyPlaylistId);
+
+    res.json({
+        resultId: spotifyPlaylistId
+    });
+});
+
+app.get('/playlist/result/:id', (req, res) => {
+    const resultId = req.params.id;
+    if (resultId !== '') {
+        if (jobs.has(resultId)) {
+            const job = jobs.get(resultId);
+            if (!job) {
+                res.json({found: true, status: 'downloading'});
+            } else {
+                res.json({found: true, status: 'complete', payload: job});
+            }
+            return;
+        }
+    }
+
+    res.json({found: false, status: null, payload: null});
+});
+
+app.delete('/playlist/result/:id', (req, res) => {
+    const resultId = req.params.id;
+    if (resultId !== '') {
+        if (jobs.has(resultId)) {
+            if (jobs.get(resultId) === false) {
+                res.status(400).json({found: true, status: 'can not delete while job is in progress'});
+            } else {
+                jobs.delete(resultId);
+                res.json({found: true, status: 'deleted successfully'});
+            }
+            return;
+        }
+    }
+
+    res.json({found: false, status: null});
 });
 
 const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
